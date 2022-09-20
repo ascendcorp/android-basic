f0a87dffd302370d5d2d318a7c7659fc6a12378c
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2022-09-20 15:24:57 +0700

    show activity lifecycle from toast

--- app/src/main/java/com/ascedncorp/androidbasic/MainActivity.kt
+++ app/src/main/java/com/ascedncorp/androidbasic/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ascedncorp.androidbasic
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ascedncorp.androidbasic.databinding.ActivityMainBinding
 
@@ -13,5 +14,37 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
